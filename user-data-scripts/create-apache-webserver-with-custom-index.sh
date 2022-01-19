22afc526b85f9e21e971ac71c614288173c265f1
Thomas Johander <tjohander@splunk.com>
2022-01-19 10:25:40 -0600

    update create-apache-webserver-with-custom-index.sh to render server metadata on index.html

--- user-data-scripts/create-apache-webserver-with-custom-index.sh
+++ user-data-scripts/create-apache-webserver-with-custom-index.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 yum update -y
-sudo amazon-linux-extras install -y lamp-mariadb10.2-php7.2 php7.2
+amazon-linux-extras install -y lamp-mariadb10.2-php7.2 php7.2
 yum install -y httpd
+
 cat >/var/www/html/index.html <<EOL
 <html>
-  <h1>Your Webserver is accessible</h1>
+  <h1>Your Webserver is accessible from $(curl http://169.254.169.254/latest/meta-data/instance-id)
+  at $(curl http://169.254.169.254/latest/meta-data/local-ipv4)</h1>
 </html>
 EOL
 systemctl start httpd
